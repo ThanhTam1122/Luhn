d2baf1c96f34c471a23f6a20aa278dd60d6c8b61
JanBigant <janborrowskigold@gmail.com>
2023-08-23 11:48:01 +0200

    feat: initial commit

---
+++ ValidatorService/src/test/java/com/jan/validator/ValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jan.validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
